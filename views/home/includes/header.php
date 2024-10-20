d525aaee9fe9390fa3f73a3c5d2b42960e1e3754
Tran Vu Hao <theflash8888@gmail.com>
2024-10-20 23:03:13 +0700

    add favicon

--- views/home/includes/header.php
+++ views/home/includes/header.php
@@ -27,6 +27,11 @@ function formatCurrency($number) {
   <meta name="keywords" content="">
   <meta name="description" content="">
 
+  <link rel="apple-touch-icon" sizes="180x180" href="<?php echo base_url('public/images/favicon/apple-touch-icon.png') ?>">
+  <link rel="icon" type="image/png" sizes="32x32" href="<?php echo base_url('public/images/favicon/favicon-32x32.png') ?>">
+  <link rel="icon" type="image/png" sizes="16x16" href="<?php echo base_url('public/images/favicon/favicon-16x16.png') ?>">
+  <link rel="manifest" href="<?php echo base_url('public/images/favicon/site.webmanifest') ?>">
+
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
   <link rel="stylesheet" type="text/css" href="<?php echo base_url('public/css/vendor.css') ?>">
