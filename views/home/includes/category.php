564bd9f540f44e10aad01a49008b47a54e2a3b98
Tran Vu Hao <theflash8888@gmail.com>
2024-10-20 23:06:00 +0700

    remove link category in home page

--- views/home/includes/category.php
+++ views/home/includes/category.php
@@ -3,12 +3,12 @@
   <div class="row">
     <?php foreach($categories as $category ): ?>
       <div class="col-md-4 col-sm-6 col-12 mb-4">
-        <a href="index.php?controller=home&action=listByCategory&categoryId=<?= $category['id'] ?>">
+        <!-- <a href="index.php?controller=home&action=listByCategory&categoryId=<?= $category['id'] ?>"> -->
           <div class="img-thumbnail" style="text-align: center;">
             <img src="<?= $category['image'] ?>" alt="<?= $category['name'] ?>" style="height: 150px;">
           </div>
           <div class="text-center"><?= $category['name'] ?></div>
-        </a>
+        <!-- </a> -->
       </div>
     <?php endforeach; ?>
   </div>
